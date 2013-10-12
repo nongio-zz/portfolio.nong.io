03874d3245dc6aa3869e836bb1758f189d615fd5
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2013-10-12 15:32:09 +0200

    working app

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
+livereload = require('express-livereload')
+
+
+
 var
   express = require('express'),
   app = express(),
   // All default options
-  poet = require('../lib/poet')(app);
+  poet = require('poet')(app);
+
 
 poet.init().then(function () {
   // initialized
@@ -14,5 +19,5 @@ app.use(express.static(__dirname + '/public'));
 app.use(app.router);
 
 app.get('/', function (req, res) { res.render('index'); });
-
+livereload(app, config={})
 app.listen(3000);
