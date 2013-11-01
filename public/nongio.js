469166d5e04a3560301ad61ccefd259755716073
Riccardo Canalicchio <riccardo.canalicchio@gmail.com>
2013-11-01 13:51:15 +0100

    better scroll

--- public/nongio.js
+++ public/nongio.js
@@ -5,47 +5,48 @@ $(document).ready(function () {
     $("#people").css({
         height:$(document).height() + "px"
     })
-    //myDataRef = new Firebase('https://nongio.firebaseio.com/users');
+
     myDataRef = new Firebase('https://nongio.firebaseio.com/users')
-
-    //var id = myDataRef.get("id") || 0;
-    myDataRef.once('value', function(allMessagesSnapshot) {
-        var current_date = (new Date()).getTime();
-        allMessagesSnapshot.forEach(function(messageSnapshot) {
-            var diff = current_date - messageSnapshot.val().date;
-            // if(diff > 36000)
-            //     messageSnapshot.ref().remove();
-
-        });
-    });
-    var current_user = myDataRef.push({
-        scroll: 0,
-        date: (new Date()).getTime(),
+    var my_color = {
         r: Math.random()*255,
         g: Math.random()*255,
         b: Math.random()*255
+    }
+    var current_user = myDataRef.push({
+        scroll: 0,
+        date: (new Date()).getTime(),
+        r: my_color.r,
+        g: my_color.g,
+        b: my_color.b
     });
-
+    var person = $("<div id=\"person_me\" class=\"person\">")
+        .css({
+            "position": "fixed",
+            "border-color": " rgb("+ Math.floor(my_color.r)+","+Math.floor(my_color.g) + ","+Math.floor(my_color.b)+")"
+        })
+    $("#people").append(person);
+    
     $(document).scroll(function(e){
         var top = $(window).scrollTop();
         current_user.update({scroll: top});
     })
 
     myDataRef.on('child_added', function(snapshot) {
-
-        var person = $("<div id=\"person"+ snapshot.name() +"\" class=\"person\">")
-            .css({top: snapshot.val().scroll+"px"})
-            .css({
-                "border-color": " transparent rgb("+ Math.floor(snapshot.val().r)+","+Math.floor(snapshot.val().g) + ","+Math.floor(snapshot.val().b)+") transparent transparent"
-            })
-        $("#people").append(person);
+       if(snapshot.name() != current_user.name()){
+            var person = $("<div id=\"person"+ snapshot.name() +"\" class=\"person\">")
+                .css({top: snapshot.val().scroll+"px"})
+                .css({
+                    "border-color": " rgb("+ Math.floor(snapshot.val().r)+","+Math.floor(snapshot.val().g) + ","+Math.floor(snapshot.val().b)+")"
+                })
+            $("#people").append(person);
+        }
 
     });
     myDataRef.on('child_changed', function(snapshot) {
-        var top = (100+snapshot.val().scroll*0.4);
+        var top = snapshot.val().scroll+100;
         $("#person"+ snapshot.name()).css({top: top+"px"})
         .css({
-            "border-color": "transparent rgb("+ Math.floor(snapshot.val().r)+","+Math.floor(snapshot.val().g) + ","+Math.floor(snapshot.val().b)+") transparent transparent"
+            "border-color": "rgb("+ Math.floor(snapshot.val().r)+","+Math.floor(snapshot.val().g) + ","+Math.floor(snapshot.val().b)+")"
         })
     });
     myDataRef.on('child_removed', function(snapshot) {
